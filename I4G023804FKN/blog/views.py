2a04dd7ebdf96b26b578dced13b1724e97b19971
Agwarota Collins <kollinzomo@gmail.com>
2022-06-22 22:49:01 +0300

    A Zuri Internship Task on Django CRUD

---
+++ I4G023804FKN/blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from .models import Post
+
+class PostListView(ListView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url  = "reverse_lazy(“blog:all”)"
+class PostDetailView(DetailView)
---
+++ README.md
@@ -0,0 +1,2 @@
+# Django-CRUD
+A task on the Zuri Internship Program
